71f39ba3c646d8566f677c1b15d421e4ef5abcb7
awa2 <tawatsu@c-fo.com>
2019-06-23 21:22:10 +0900

    batchAppからミドルウェアを分離

--- app/batch.ts
+++ app/batch.ts
@@ -1,8 +1,13 @@
 import { BatchApp, process, md5sum } from '@ts-module-for-gas/gas-utilities';
 import { Slack, Attachment } from '@ts-module-for-gas/gas-slack';
-
+import gmailToSlackAttachment from './batch/gmailToSlackAttachment';
 const INTERVAL = 5;
 
+
+// Batch proccessing application
+// NOTE : This generates regulary scheduled function by GAS time-based trigger.
+//        It uses for creating event-driven function.
+//
 function batchApp() {
 
     // [SETTINGS]
@@ -15,27 +20,12 @@ function batchApp() {
 
     const app = new BatchApp();
 
-    // [SETTINGS]
-    // CONDITIONS_FOR_MAIL : Set gmail search conditions, such as 'from:(github.com)', with Array(string[]);
-    // 
-    const ConditionsForMail = process.env['CONDITIONS_FOR_MAIL'] as string[];
-    ConditionsForMail.map(condition => {
-        app.onGmailReceived(condition, (message) => {
-            const attachment: Attachment = {
-                title: message.getSubject(),
-                title_link: message.getThread().getPermalink(),
-                text: message.getPlainBody(),
-                fields: [
-                    { title: 'To', value: message.getTo(), short: true },
-                    { title: 'From', value: message.getFrom(), short: true },
-                ],
-                footer: Utilities.formatDate(message.getDate(), 'JST', 'yyyy/MM/dd hh:mm:ss')
-            };
-            const cc = message.getCc();
-            if (cc) { attachment.fields.push({ title: 'CC', value: cc, short: true }) }
-            const bcc = message.getBcc();
-            if (bcc) { attachment.fields.push({ title: 'BCC', value: bcc, short: true }) }
-
+    // [SETTING EXAMPLE]
+    // CONDITIONS_FOR_MAIL : Set gmail search conditions, such as 'from:(github.com)', with Array( { condition: 'from:(github.com)'} );
+    const ConditionsForMail = process.env['CONDITIONS_FOR_MAIL'] as { condition: string}[];
+    ConditionsForMail.map(conditionForMail => {
+        app.onGmailReceived(conditionForMail.condition, (message) =>{
+            const attachment = gmailToSlackAttachment(message);
             SlackBot.post(channel, attachment);
         });
     })
---
+++ app/batch/gmailToSlackAttachment.ts
@@ -0,0 +1,20 @@
+import { Attachment } from '@ts-module-for-gas/gas-slack';
+
+export default (message: GoogleAppsScript.Gmail.GmailMessage) => {
+    const attachment: Attachment = {
+        title: message.getSubject(),
+        title_link: message.getThread().getPermalink(),
+        text: message.getPlainBody(),
+        fields: [
+            { title: 'To', value: message.getTo(), short: true },
+            { title: 'From', value: message.getFrom(), short: true },
+        ],
+        footer: Utilities.formatDate(message.getDate(), 'JST', 'yyyy/MM/dd hh:mm:ss')
+    };
+    const cc = message.getCc();
+    if (cc) { attachment.fields.push({ title: 'CC', value: cc, short: true }) }
+    const bcc = message.getBcc();
+    if (bcc) { attachment.fields.push({ title: 'BCC', value: bcc, short: true }) }
+
+    return attachment;
+}
