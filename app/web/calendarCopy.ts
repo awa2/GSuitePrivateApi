7bfaa104da9a9a78fbbeb42e44484b2d2a881647
awa2 <tawatsu@c-fo.com>
2019-06-24 00:48:25 +0900

    Namespace added

--- app/web/calendarCopy.ts
+++ app/web/calendarCopy.ts
@@ -1,52 +1,90 @@
 import { Request, Response } from '@ts-module-for-gas/gas-http';
 import { process } from '@ts-module-for-gas/gas-utilities';
-
-export default (req: Request, res: Response) => {
-    const condition = req.body['condition'] ? req.body['condition'] as {
+namespace CalendarCopy {
+    export type Query = { calendar: 'copy' }
+    export type Body = {
+        Title: string,
+        Description: string,
+        StartTime: string,
+        EndTime: string,
+        ResponseStatus: string,
+        EventUrl: string,
+        condition: Condition
+    }
+    export type Condition = {
         ifStartedAfter?: string,
         ifHoliday?: boolean,
         ifWorkday?: boolean,
         ifWordInclude?: string
-    } : {}
-    const day = new Date().getDay()
+    }
+    export const main = (req: Request<Query, Body>, res: Response) => {
+        if (req.body.ResponseStatus === 'Accepted') {
+            const condition: Condition = req.body['condition'] ? req.body.condition : {}
+            const StartTime = new Date(req.body.StartTime) ? new Date(req.body.StartTime) : dateParse(req.body.StartTime);
+            const EndTime = new Date(req.body.EndTime) ? new Date(req.body.EndTime) : dateParse(req.body.EndTime);
 
-    let startedAfter: number;
-    if (condition.ifStartedAfter) {
-        if (condition.ifStartedAfter.match(/[0-2][0-9][0-5][0-9]/) && condition.ifStartedAfter.length === 4) {
-            startedAfter = parseInt(condition.ifStartedAfter);
-
-            if (startedAfter <= parseInt(Utilities.formatDate(new Date(), 'JST', 'hhmm'))) {
-                const event = CalendarCreate(req.body);
-                res.send({ status: 'OK', id: event.getId(), condition: { ifStartedAfter: { startedAfter } } });
+            if (condition.ifStartedAfter) {
+                let startedAfter: number;
+                if (condition.ifStartedAfter.match(/[0-2][0-9][0-5][0-9]/) && condition.ifStartedAfter.length === 4) {
+                    startedAfter = parseInt(condition.ifStartedAfter);
+                    if (startedAfter <= parseInt(Utilities.formatDate(StartTime, 'JST', 'hhmm'))) {
+                        const event = CalendarCreate(req.body as Body, StartTime, EndTime);
+                        res.send({ status: 'OK', id: event.getId(), condition: { ifStartedAfter: { startedAfter } } });
+                        return
+                    }
+                }
+            }
+            if (condition.ifHoliday && (StartTime.getDay() === 0 || StartTime.getDay() === 6)) {
+                const event = CalendarCreate(req.body as Body, StartTime, EndTime);
+                res.send({ status: 'OK', id: event.getId(), condition: { ifHoliday: true } });
+                return
+            }
+            if (condition.ifWorkday && (0 < StartTime.getDay() && StartTime.getDay() < 6)) {
+                const event = CalendarCreate(req.body as Body, StartTime, EndTime);
+                res.send({ status: 'OK', id: event.getId(), condition: { ifWorkday: true } });
+                return
+            }
+            if (condition.ifWordInclude) {
+                const title = req.body['title'] as string;
+                if (title.includes(condition.ifWordInclude)) {
+                    const event = CalendarCreate(req.body as Body, StartTime, EndTime);
+                    res.send({ status: 'OK', id: event.getId(), condition: { ifWordInclude: condition.ifWordInclude } });
+                    return
+                }
             }
         }
-    }
-    if (condition.ifHoliday && (day === 0 || day === 6)) {
-        const event = CalendarCreate(req.body);
-        res.send({ status: 'OK', id: event.getId(), condition: { ifHoliday: true } });
-    }
-    if (condition.ifWorkday && (0 < day && day < 6)) {
-        const event = CalendarCreate(req.body);
-        res.send({ status: 'OK', id: event.getId(), condition: { ifWorkday: true } });
-    }
-    if (condition.ifWordInclude) {
-        const title = req.body['title'] as string;
-        if (title.includes(condition.ifWordInclude)) {
-            const event = CalendarCreate(req.body);
-            res.send({ status: 'OK', id: event.getId(), condition: { ifWordInclude: condition.ifWordInclude } });
-        }
-    }
-    process.log({ status: 'No event copied' });
-    res.send({ status: 'No event copied' });
-}
 
-function CalendarCreate(option: any) {
-    process.log({ CalendarCreate: option });
-    return CalendarApp.createEvent(option['title'] as string,
-        option['startTime'],
-        option['endTime'],
-        {
-            description: option['description']
+        process.log({ status: 'No event copied' });
+        res.send({ status: 'No event copied' });
+        return
+    }
+
+    export function CalendarCreate(option: { Title: string, Description: string }, startTime: Date, endTime: Date) {
+        process.log({ CalendarCreate: option });
+        return CalendarApp.createEvent(option.Title,
+            startTime,
+            endTime,
+            {
+                description: option.Description
+            }
+        );
+    }
+
+    export function dateParse(str: string) {
+        const dateTime = new Date(str.replace('at', '').replace('AM', '').replace('PM', ''))
+        if (str.includes('PM')) {
+            return new Date(
+                dateTime.getFullYear(),
+                dateTime.getMonth(),
+                dateTime.getDate(),
+                dateTime.getHours() + 12,
+                dateTime.getMinutes(),
+                dateTime.getSeconds()
+            )
+        } else {
+            return new Date();
         }
-    );
+    }
 }
+const calendarCopy = CalendarCopy.main
+export default calendarCopy;
